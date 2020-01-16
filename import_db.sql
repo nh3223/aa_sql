21cf1e9cf42fc066d349ebb4acd38de84e2ce7e4
nh3223 <nhudders@gmail.com>
2020-01-16 10:28:49 -0800

    Fixed some errors. Added additional seed. Created database.

--- import_db.sql
+++ import_db.sql
@@ -22,8 +22,8 @@ CREATE TABLE questions (
 );
 
 CREATE TABLE question_follows (
-    user_id     INTEGER PRIMARY KEY,
-    question_id INTEGER PRIMARY KEY,
+    user_id     INTEGER,
+    question_id INTEGER,
 
     FOREIGN KEY (user_id)     REFERENCES  users(id),
     FOREIGN KEY (question_id) REFERENCES  questions(id)
@@ -37,12 +37,12 @@ CREATE TABLE replies (
     body            TEXT    NOT NULL,
 
     FOREIGN KEY (question_id) REFERENCES  questions(id),
-    FOREIGN KEY (parent_id)   REFERENCES  replies(id)
+    FOREIGN KEY (parent_reply_id)   REFERENCES  replies(id)
 );
 
 CREATE TABLE question_likes (
-    user_id     INTEGER PRIMARY KEY,
-    question_id INTEGER PRIMARY KEY,
+    user_id     INTEGER,
+    question_id INTEGER,
     likes       BOOLEAN NOT NULL,
 
     FOREIGN KEY (user_id)     REFERENCES  users(id),
@@ -62,6 +62,15 @@ INSERT INTO
 VALUES
     ('Les Miserables','Who am I?', (SELECT id FROM users WHERE last_name = 'Valjean')),
     ('Chicken','Why did the chicken cross the road?', (SELECT id FROM users WHERE last_name = 'Doe'));
+
+INSERT INTO
+    question_follows(user_id,question_id)
+VALUES
+    (1,1),
+    (2,1),
+    (3,1),
+    (3,2),
+    (4,2);
 
 INSERT INTO
     replies(question_id,parent_reply_id,user_id,body)
